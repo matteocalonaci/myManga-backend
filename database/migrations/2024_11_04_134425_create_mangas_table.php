a28ccce1283b303bde73f1c114956b4d8c09ee27
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-11-05 11:50:56 +0100

    Migrazioni

---
+++ database/migrations/2024_11_04_134425_create_mangas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mangas', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->text('description');
+            $table->string('cover_image');
+            $table->decimal('price', 8, 2);
+            $table->boolean('availability')->default(true);
+            $table->year('year');
+            $table->string('slug')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mangas');
+    }
+};
