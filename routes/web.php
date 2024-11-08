831ed9e410201c0476c16cdffdbcab54d3356b30
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-11-08 15:33:35 +0100

    reso inaccessibile la rotta register inquanto inutile (al momento)

--- routes/web.php
+++ routes/web.php
@@ -3,32 +3,27 @@
 use App\Http\Controllers\Admin\AuthorController;
 use App\Http\Controllers\Admin\CategoryController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\DashboardController; //<---- Import del controller precedentemente creato!
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\EditorController;
 use App\Http\Controllers\Admin\GenreController;
 use App\Http\Controllers\Admin\MangaController;
-use App\Models\Category;
-use App\Models\Genre;
-use Database\Seeders\EditorSeeder;
+use Illuminate\Support\Facades\Auth;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// ...
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Disabilita la registrazione prima di includere auth.php
+Auth::routes(['register' => false]); // Disabilita la registrazione
+Route::get('/register', function () {
+    return redirect('/'); // Reindirizza alla home
+});
+
 Route::middleware(['auth'])
-    ->prefix('admin') // Definisce il prefisso "admin/" per le rotte di questo gruppo
-    ->name('admin.') // Definisce il pattern con cui generare i nomi delle rotte cioè "admin.qualcosa"
+    ->prefix('admin')
+    ->name('admin.')
     ->group(function () {
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
         Route::resource('/mangas', MangaController::class);
@@ -36,11 +31,7 @@ Route::middleware(['auth'])
         Route::get('/search-categories', [CategoryController::class, 'searchCategories']);
         Route::get('/search-genres', [GenreController::class, 'searchGenres']);
         Route::get('/search-editors', [EditorController::class, 'searchEditors']);
-
-
     });
 
+// Includi auth.php dopo aver disabilitato la registrazione
 require __DIR__ . '/auth.php';
-
-
-
