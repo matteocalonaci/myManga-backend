bfc2715594fc975fcc6d6ba53709ba62bb94f07e
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-11-28 16:44:47 +0100

    mod chiamata api index

--- app/Http/Controllers/Api/MangaController.php
+++ app/Http/Controllers/Api/MangaController.php
@@ -4,14 +4,40 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Manga;
+use App\Models\Author;
+use App\Models\Genre;
+use App\Models\Category;
+use App\Models\Editor;
+use Illuminate\Http\Request;
+
 
 class MangaController extends Controller
 {
-    public function index(){
+    public function index(Request $request) {
+        // Recupera i parametri di paginazione
+        $perPage = $request->input('per_page', 12); // Imposta il numero di risultati per pagina, default a 12
+
+        // Recupera i manga con paginazione e carica i generi associati
+        $mangas = Manga::with('genres')->paginate($perPage);
+
+        // Recupera gli autori, generi, categorie ed editor
+        $authors = Author::all();
+        $genres = Genre::all();
+        $categories = Category::all();
+        $editors = Editor::all();
+
         return response()->json([
-            'success'=> true,
-            //with per pescare anche i generi
-            'mangas' =>  Manga::with('genres')->orderBy('id')->paginate(12)
+            'success' => true,
+            'mangas' => [
+                'data' => $mangas->items(), // I dati della pagina corrente
+                'current_page' => $mangas->currentPage(), // Pagina corrente
+                'last_page' => $mangas->lastPage(), // Ultima pagina
+                'total' => $mangas->total(), // Totale elementi
+            ],
+            'authors' => $authors,
+            'genres' => $genres,
+            'categories' => $categories,
+            'editors' => $editors,
         ]);
     }
 
